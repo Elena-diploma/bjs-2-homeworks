6cdba9ebc69a56e9147bdf13dfe7515d69b5ded0
Elena Tursunova <elenatursunova@gmail.com>
2021-07-03 15:00:49 +0300

    домашнее задание к лекции Расширенная работа с массивами

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,22 @@
+
+//Тут логично бы было просто написать return, но начальное тело функции менять не стала
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+    let result = Array.isArray(arr1)
+        && Array.isArray(arr2)
+        && arr1.length === arr2.length
+        && arr1.every((item, index) => {
+            return item === arr2[index]
+        });
+    return result;
 }
 
+//Та же фигня
 function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
-
-  return resultArr; // array
+    let resultArr = arr.filter((item) => {
+        return item > 0 && item % 3 === 0
+    }).map((item) => {
+        return item * 10
+    });
+    return resultArr; // array
 }
