e67930800848b42c7cce560cb868c97955a4d14a
Elena Tursunova <elenatursunova@gmail.com>
2021-07-22 17:20:54 +0300

    исправлено

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,26 +1,26 @@
-// // задание1
-// function cachingDecoratorNew(func) {
-//     let cache = [];
-//     function wrapper(...args) {
-//         const hash = args.join(",");
-//         let idx = cache.findIndex((item) => item.hash === hash);
-//         console.log(idx);
-//         if (idx !== -1 ) {
-//             console.log("Из кеша: " + cache[idx].value);
-//             return "Из кэша: " + cache[idx].value;
-//         } else {
-//             let result = func.apply(this, args);
-//             cache.push({"hash": hash, "value": result});
-//
-//             if (cache.length > 5) {
-//                 cache.splice(0,1);
-//             }
-//             console.log("Вычисляем: " + result);
-//             return "Вычисляем: " + result;
-//         }
-//     }
-//     return wrapper;
-// }
+// задание1
+function cachingDecoratorNew(func) {
+    let cache = [];
+    function wrapper(...args) {
+        const hash = args.join(",");
+        let idx = cache.findIndex((item) => item.hash === hash);
+        console.log(idx);
+        if (idx !== -1 ) {
+            console.log("Из кеша: " + cache[idx].value);
+            return "Из кэша: " + cache[idx].value;
+        } else {
+            let result = func.apply(this, args);
+            cache.push({"hash": hash, "value": result});
+
+            if (cache.length > 5) {
+                cache.splice(0,1);
+            }
+            console.log("Вычисляем: " + result);
+            return "Вычисляем: " + result;
+        }
+    }
+    return wrapper;
+}
 
 // задание2
 
